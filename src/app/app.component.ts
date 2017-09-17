ce7341f6f21af0a253b4705e52cee95a06492525
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-09-17 17:29:53 +0200

    Clicking on heroes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,11 +23,13 @@ const HEROES: Hero[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
